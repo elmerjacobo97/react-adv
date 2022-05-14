f63ea91d02ffece0227330a7c0cccfd5d3a0809e
Elmer Jacobo <ejacobotiniano@gmail.com>
2022-05-14 11:09:20 -0500

    Component Extend Styles - listo

--- src/02-component-patterns/components/ProductButtons.tsx
+++ src/02-component-patterns/components/ProductButtons.tsx
@@ -1,12 +1,20 @@
-import { useContext } from "react";
+import { CSSProperties, useContext } from "react";
 import styles from "../styles/styles.module.css";
 import { ProductContext } from "./ProductCard";
 
-export const ProductButtons = () => {
+export interface Props {
+    className?: string;
+    style?: CSSProperties;
+}
+
+export const ProductButtons = ({ className, style }: Props) => {
     const { counter, increaseBy} = useContext(ProductContext);
 
     return (
-        <div className={styles.buttonsContainer}>
+        <div
+            className={`${styles.buttonsContainer} ${className}`}
+            style={style}
+        >
             <button
                 type="button"
                 className={styles.buttonMinus}
