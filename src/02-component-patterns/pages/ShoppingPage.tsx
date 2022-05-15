76444ef4bf357a71d0620f55f8478f8e222a0976
Elmer Jacobo <ejacobotiniano@gmail.com>
2022-05-15 12:48:26 -0500

    pre-npm listo

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,6 +1,5 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components";
 import { products } from "../data/products";
-import '../styles/custom-styles.css';
 
 const product = products[0];
 
@@ -14,32 +13,17 @@ export const ShoppingPage = () => {
             <ProductCard
                 key={product.id}
                 product={product}
-                className="bg-dark text-white"
                 initialValues={{
                     count: 4,
-                    maxCount: 10
+                    // maxCount: 10
                 }}
             >
                 {
                     ({ reset, isMaxCountReached, count, maxCount, increaseBy }) => (
                         <>
-                            <ProductImage
-                                className="custom-image"
-                                style={{
-                                    boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'
-                                }}
-                            />
-                            <ProductTitle className="text-bold" />
-                            <ProductButtons className="custom-buttons" />
-
-                            <button type="button" onClick={reset}>Reset</button>
-                            <button type="button" onClick={() => increaseBy(-2)}>-2</button>
-                            {
-                                ( !isMaxCountReached &&
-                                    <button type="button" onClick={() => increaseBy(+2)}>+2</button>
-                                )
-                            }
-                            <span>{count} - {maxCount}</span>
+                            <ProductImage />
+                            <ProductTitle />
+                            <ProductButtons />
                         </>
                     )
                 }
